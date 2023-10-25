64b6c3ff7ab13e9ece803bc99a1d207b99b0b682
Kyymo <137647536+MehdiAlani@users.noreply.github.com>
2023-10-25 16:48:46 -0200

    25 oct 23

--- 1_Two_Sum.c
+++ 1_Two_Sum.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int * Result = malloc(2 * sizeof(int));
+    for(int i = 0; i < numsSize; i++){
+        for(int j = 0; j < numsSize && j !=i; j++){
+            if(nums[i] + nums[j] == target){
+                Result[0]=i;
+                Result[1]=j;
+            }
+        }   
+    }
+    return Result;
+} 
 int main(){
-    
-
+    int Par[] = {3,2,4};
+    int * Result = twoSum(Par,3,5,Par);
+        printf("%d ",Result[1]);
     return 0;
 }
